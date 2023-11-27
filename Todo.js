b694b6e05276122824aee596d072f2e4d655b730
Ansh Wasan <69054054+FrostyFeet-coder@users.noreply.github.com>
2023-11-27 14:21:04 +0530

    Updated Todo.js

--- Todo.js
+++ Todo.js
@@ -22,46 +22,6 @@ function addTodo(){
     console.log("hello world")
 }
 
-// function displayitem(){
-//    let containerElement =document.querySelector(".todo-container");
-//    let newHtml = '';
-//    for (let i =0 ;i<todolist.length ; i++){
-//        let {item, duedate} = todolist[i];
-//        newHtml +=`
-//          <span>${item}</span>
-//          <span>${duedate}</span>
-//          <button onclick="todolist.splice(${i},1) ;
-//        displayitem();">Delete</buton>
-//        `;
-//        
-//    }
-//    containerElement.innerHTML = newHtml;
-// }
-
-// let todoList = [
-//     {
-//       item: 'Buy Milk',
-//       dueDate: '4/10/2023'
-//     },
-//     {
-//       item: 'Go to College',
-//       dueDate: '4/10/2023'
-//     }
-//   ];
-  
-//   displayItems();
-  
-//   function addTodo() {
-//     let inputElement = document.querySelector('#todo-input');
-//     let dateElement = document.querySelector('#todo-date');
-//     let todoItem = inputElement.value;
-//     let todoDate = dateElement.value;
-//     todoList.push({item: todoItem, dueDate: todoDate});
-//     inputElement.value = '';
-//     dateElement.value = '';
-//     displayItems();
-//   }
-  
   function displayitem() {
     let containerElement = document.querySelector('.todo-container');
     let newHtml = '';
